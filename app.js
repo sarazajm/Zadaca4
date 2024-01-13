8a7415fce69e7f94fba5220f190f3edb7d056307
sarazajm <sarazajmovic2@gmail.com>
2024-01-13 21:37:43 +0100

    Zadatak3

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ dugme.addEventListener("click", ispisati);
 function ispisati(){
     let noviElm = document.createElement("p");
     noviElm.innerHTML = '"Kada želimo pružiti hitno potrebnu potporu, od vitalnog je značaja da razmotrimo i pružanje dugoročne podrške kako bismo pomogli stanovništvu da si pomogne”, rekao je Yousef."';
-    noviElm.className = "fw-light lh-sm";
+    noviElm.className = "fw-light";
     document.getElementById("head-col").appendChild(noviElm);
+}
+
+function ispisiRijec(rijec){
+    let r = document.createElement("t");
+    r.innerHTML = "Vasa rijec za pretragu je: "+rijec;
+    document.getElementById("head-col").appendChild(r);
 }
